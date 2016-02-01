b5b02e0b95e95c1b047859fd4270b0b8dcd3c3f1
julien.deweweire <julien.deweweire@gmail.com>
2016-02-01 12:09:11 +0100

    Search component not finish + list cities json

--- src/Search.js
+++ src/Search.js
@@ -3,7 +3,15 @@ var React = require('react');
 var BasicSearch= React.createClass({
   render : function(){
     return (
+      <div>
      <h1>basicSearch</h1>
+     <div class="form-group">
+    <label for="usr">email:</label>
+    <input type="email" class="form-control" id="email"/>
+  </div>
+  </div>
     );
   }
 });
+
+module.exports = BasicSearch;
