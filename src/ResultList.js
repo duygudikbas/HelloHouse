16c4bb5f982c58e633942eabfbadde28f1d335b4
Marie-Eve <mebeeckman@gmail.com>
2016-02-02 14:01:05 +0100

    m

--- src/ResultList.js
+++ src/ResultList.js
@@ -2,8 +2,6 @@ var React = require('react');
 var $ = require('jquery');
 var Link = require('react-router').Link
 
-
-
 var AResult = React.createClass({
 render: function(){
     var urlTo = "/PropertyDetail/"+ this.props.estate.id;
@@ -14,17 +12,38 @@ render: function(){
         <li className="liResultList">
           <p className="location"> {this.props.estate.location} </p>
           <p className="price">&nbsp;{price}</p>
-          <img className="imgResultList" src={this.props.estate.image}/>
+          <img className="imgResultList" src={this.props.estate.image[0]}/>
         </li>
       </Link>
     );
   }
 });
 
+
+var Warning = React.createClass({
+  render: function(){
+    return (
+        <p>May be could you adapt your <Link to="/Search" >Search</Link></p>
+      );}
+  });
+
+var NumberWarning = React.createClass({
+  render: function(){
+    var number = this.props.numberWarning;
+    var warning = "";
+
+    return(
+      <div>
+        <p>Number of results : {number}</p>
+        {this.props.numberWarning > 50 ? <Warning/> : null}
+      </div>
+    );
+  }
+});
+
 var ResultList = React.createClass({
   getInitialState: function() {
-    return {estates: [
-    ]};
+    return {estates: []};
   },
 
   componentDidMount: function() {
@@ -34,9 +53,17 @@ var ResultList = React.createClass({
     }.bind(this), 'json');
   },
 
+  filter : function(estate){
+    if (this.props.filter.minPrice > estate.price){ return false; };
+    if (this.props.filter.maxPrice < estate.price){ return false; };
+    return true;
+  },
+
   render: function(){
     console.log(this.state.estates);
-       var resultElems = this.state.estates.map(function(estate) {
+    var filteredEstates = this.state.estates.filter(this.filter);
+    var numberWarning = filteredEstates.length;
+    var resultElems = filteredEstates.map(function(estate) {
       return (
         <AResult key={estate.id} estate={estate} />
        );
@@ -44,6 +71,7 @@ var ResultList = React.createClass({
     return(
       <div className="container">        
          <h1>Propositions</h1>
+         <NumberWarning numberWarning = {numberWarning}/>
          <ul className="ulResultList">
          {resultElems }
          </ul> 
